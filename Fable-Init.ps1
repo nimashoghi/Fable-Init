2cd6b5b1ade15b7a36de91884c5fc92eadfac5f6
Nima Shoghi <nimashoghi@gmail.com>
2018-11-24 17:51:34 -0500

    Added Plaster-based setup

--- Fable-Init.ps1
+++ Fable-Init.ps1
@@ -1,28 +1,7 @@
 param (
     [Parameter(Mandatory)]
     [string]
-    $Name,
-
-    [int]
-    $DefaultPort = 61225,
-
-    [string]
-    $FableToolsVersion = "1.3.3"
+    $Path
 )
 
-Import-Module ".\Build.psm1"
-Import-Module ".\Dotnet.psm1"
-Import-Module ".\Node.psm1"
-
-# create directory
-New-Item -ItemType Directory -Path $Name
-
-Push-Location "$Name"
-
-# setup node project
-Init-Node-Project -Name $Name -Path ".\"
-
-# setup dotnet project
-New-Fable-Project -Name $Name -Path ".\"
-
-Pop-Location
+Invoke-Plaster -TemplatePath "$PSScriptRoot" -DestinationPath "$Path"
--- _FinalizeSetup.ps1
+++ _FinalizeSetup.ps1
@@ -1,1 +1,15 @@
-dotnet restore
+Begin {
+    Push-Location "$PSScriptRoot"
+}
+Process {
+    # dotnet new sln | Out-Null
+    # dotnet sln add <%= $PLASTER_PARAM_Name -replace " ", "_" %>.fsproj | Out-Null
+    dotnet restore | Out-Null
+    yarn | Out-Null
+    yarn add --dev @babel/core fable-splitter | Out-Null
+    Remove-Item -Force "$PSScriptRoot/FinalizeSetup.ps1"
+}
+End {
+    Pop-Location
+}
+
